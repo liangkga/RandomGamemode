ddf17b3e4ea8e01eb91040d7fe7490e0f193cff6
liangkga <3694851224@qq.com>
2025-05-25 03:04:37 +0800

    Update ForceGamemode.cs

--- RandomGamemode/Commands/ForceGamemode.cs
+++ RandomGamemode/Commands/ForceGamemode.cs
@@ -1,1 +1,45 @@
+using CommandSystem;
+using System;
 
+namespace RandomGamemode.Commands
+{
+	[CommandHandler( typeof( RemoteAdminCommandHandler ) )]
+	[CommandHandler( typeof( GameConsoleCommandHandler ) )]
+	internal sealed class ForceGamemode : ICommand
+	{
+		public string Command { get; } = "forcegamemode";
+		public string[] Aliases { get; } = Array.Empty<string>();
+		public string Description { get; } = "Forces the next round to be a special gamemode.";
+		public bool Execute( ArraySegment<string> arguments, ICommandSender sender, out string response )
+		{
+			if ( !sender.CheckPermission( PlayerPermissions.GameplayData ) )
+			{
+				response = "<color=red>你没有权限使用此命令！</color>";
+				return false;
+			}
+
+			if ( arguments.Count < 1 )
+			{
+				Plugin.NextGamemode = Plugin.EnabledList.RandomItem();
+				response = $"The {Plugin.GetGamemodeName( Plugin.NextGamemode )} gamemode will be activated next round.";
+				return true;
+			}
+
+			try
+			{
+				Gamemode selectedGamemode = ( Gamemode ) int.Parse( arguments.At( 0 ) );
+				Plugin.NextGamemode = selectedGamemode;
+				if ( Plugin.NextGamemode == Gamemode.Invalid )
+					response = "Manual gamemode override for next round has been disabled.";
+				else
+					response = $"{Plugin.GetGamemodeName( Plugin.NextGamemode )} 游戏模式将在下一回合激活。";
+				return true;
+			}
+			catch
+			{
+				response = $"<color=red>Argument must be a number that corresponds to the gamemode you want to select.</color>";
+				return false;
+			}
+		}
+	}
+}
